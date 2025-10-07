dc686e2bf14e4a432b1fd8ca7b14b5c98f792886
Fatemeh <ftmhbesinuss@gmail.com>
2025-10-07 20:03:28 +0330

    feat(layout): add layout component for authentication

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -4,18 +4,17 @@ import Image from "next/image";
 import LoginImage from "@/assets/login-image.jpg";
 import LoginForm from "./LoginForm";
 
-
 export const metadata: Metadata = {
   title: "Login",
 };
 
 export default function Login() {
   return (
-    <main className="flex justify-center items-center p-5 h-screen">
-      <div className="flex bg-card shadow-2xl rounded-2xl w-full max-w-[64rem] h-full max-h-[40rem] overflow-hidden">
-        <div className="space-y-10 p-10 w-full md:w-1/2 overflow-y-auto">
+    <main className="flex h-screen items-center justify-center p-5">
+      <div className="flex h-full max-h-[40rem] w-full max-w-[64rem] overflow-hidden rounded-2xl bg-card shadow-2xl">
+        <div className="w-full space-y-10 overflow-y-auto p-10 md:w-1/2">
           <div className="space-y-1 text-center">
-            <h1 className="font-bold text-3xl">Login to Liora</h1>
+            <h1 className="text-3xl font-bold">Login to Liora</h1>
           </div>
           <div className="space-y-5">
             <LoginForm />
@@ -27,10 +26,9 @@ export default function Login() {
         <Image
           src={LoginImage}
           alt="Login"
-          className="hidden md:block w-1/2 object-cover"
+          className="hidden w-1/2 object-cover md:block"
         />
       </div>
     </main>
   );
 }
-
