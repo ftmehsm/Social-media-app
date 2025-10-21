09afc2c5b3c2bc36f56f40e3b802f36219113f2b
Fatemeh <ftmhbesinuss@gmail.com>
2025-10-21 19:55:13 +0330

    feat(menu-bar):create the menubar of main page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,5 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
-  return <main>front page</main>;
+  return <main className="h-[200vh] w-full bg-red-50">
+    <div className="w-full">
+      frontend page
+    </div>
+  </main>;
 }
