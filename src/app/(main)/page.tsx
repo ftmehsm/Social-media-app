317f55796710fd91ae2805b40cbab6321f4e1771
Fatemeh <ftmhbesinuss@gmail.com>
2025-10-23 12:48:03 +0330

    feat(post-editor): add post editor and handle its functionality

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,7 +1,9 @@
+import PostEditor from "@/components/posts/editor/PostEditor";
+
 export default function Home() {
   return <main className="h-[200vh] w-full bg-red-50">
     <div className="w-full">
-      frontend page
+      <PostEditor/>
     </div>
   </main>;
 }
